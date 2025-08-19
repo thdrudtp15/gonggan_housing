d13f59d20ff0d5a42fa03ac20b637bad12f2cf6c
thdrudtp15 <15rudtp@naver.com>
2025-08-19 20:14:46 +0900

    feat : 포트폴리오 페이지 데이터 요청
    feat : 페이지네이션 기능

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,5 +1,42 @@
-const HomePage = () => {
-    return <div></div>;
+import Pagination from '@/components/ui/Pagination';
+import { createClient } from '@/utils/supabase/server';
+import { unstable_cache } from 'next/cache';
+import Link from 'next/link';
+
+type Props = {
+    searchParams: Promise<{ page: string; search: string }>;
+};
+
+const HomePage = async ({ searchParams }: Props) => {
+    const { page, search } = await searchParams;
+
+    const pageSize = 2;
+    const from = ((+page || 1) - 1) * pageSize;
+    const to = from + pageSize - 1;
+
+    console.log(from, to);
+
+    const supabase = await createClient();
+    const { data, count } = await supabase
+        .from('portfolio')
+        .select('*', { count: 'exact' })
+        .order('created_at', { ascending: false })
+        .range(from, to);
+
+    const totalPages = (count && Math.ceil(count / pageSize)) || 1;
+
+    return (
+        <div>
+            <ul>
+                {data?.map((portfolio) => (
+                    <li key={portfolio.id}>
+                        <Link href={`/portfolio/${portfolio.id}`}>{portfolio.title}</Link>
+                    </li>
+                ))}
+            </ul>
+            <Pagination totalPages={totalPages} nowPage={page} />
+        </div>
+    );
 };
 
 export default HomePage;
--- components/ui/Pagination.tsx
+++ components/ui/Pagination.tsx
@@ -1,3 +1,64 @@
-const Pagination = () => {};
+import { notFound } from 'next/navigation';
+
+import Link from 'next/link';
+import styles from './Pagination.module.scss';
+
+type Props = {
+    totalPages: number;
+    nowPage: string | number;
+};
+
+const Pagination = ({ totalPages, nowPage = 1 }: Props) => {
+    nowPage = +nowPage;
+
+    if (nowPage > totalPages) {
+        notFound();
+    }
+
+    const sequentialPagination = (type: 'prev' | 'next') => {
+        if (type === 'next' && nowPage < totalPages) {
+            return `?page=${nowPage + 1}`;
+        } else if (type === 'prev' && nowPage !== 1) {
+            return `?page=${nowPage - 1}`;
+        }
+        return '';
+    };
+
+    const pageLimit = 5; // 페이지 네이션 표시 개수
+    const pageArray: number[] = [];
+
+    const index = Math.floor((nowPage - 1) / pageLimit);
+
+    for (let i = index * pageLimit; i < (index + 1) * pageLimit; i++) {
+        if (i < totalPages) pageArray.push(i + 1);
+    }
+
+    return (
+        <div className={styles.pagination}>
+            {nowPage !== 1 && (
+                <Link href={sequentialPagination('prev')} className={styles.sequential}>
+                    Previous
+                </Link>
+            )}
+            <ul className={styles.pageList}>
+                {pageArray.map((page) => (
+                    <li key={page}>
+                        <Link
+                            className={`${styles.pageItem} ${nowPage === page ? styles.select : ''}`}
+                            href={`?page=${page}`}
+                        >
+                            {page}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            {nowPage < totalPages && (
+                <Link href={sequentialPagination('next')} className={styles.sequential}>
+                    Next
+                </Link>
+            )}
+        </div>
+    );
+};
 
 export default Pagination;
